aa4f5c085360a2cb5ab66323d29e5d2675c1d9a9
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2021-08-25 13:43:38 -0400

    Core Application: added custom responses

---
+++ GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCategoryCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using GloboTicket.TicketManagement.Application.Responses;
+using System.Collections.Generic;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands
+{
+    public class CreateCategoryCommandResponse:BaseResponse
+    {
+        public CreateCategoryDto Category { get; set; }
+        public CreateCategoryCommandResponse():base()
+        {
+
+        }
+    }
+}
