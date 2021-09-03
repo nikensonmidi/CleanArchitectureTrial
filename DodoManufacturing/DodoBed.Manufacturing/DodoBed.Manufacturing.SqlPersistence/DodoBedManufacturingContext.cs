2758548c65104c32dc287f602787d19bd232672f
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2021-09-03 12:34:55 -0400

    Infrastructure - Persistence: added dbset

---
+++ DodoManufacturing/DodoBed.Manufacturing/DodoBed.Manufacturing.SqlPersistence/DodoBedManufacturingContext.cs
@@ -0,0 +1,40 @@
+﻿using DodoBed.Manufacturing.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodoBed.Manufacturing.SqlPersistence
+{
+    public class DodoBedManufacturingContext:DbContext
+    {
+        public DodoBedManufacturingContext(DbContextOptions<DodoBedManufacturingContext> options):base(options)
+        {
+
+        }
+        //Was used to initialize the database
+        //public class AppDbContextFactory : IDesignTimeDbContextFactory<GloboTicketDBcontext>
+        //{
+        //    public GloboTicketDBcontext CreateDbContext(string[] args)
+        //    {
+        //        var optionsBuilder = new DbContextOptionsBuilder<GloboTicketDBcontext>();
+        //        optionsBuilder.UseSqlServer("");
+
+        //        return new GloboTicketDBcontext(optionsBuilder.Options);
+        //    }
+        //}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Tool> Tools { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<DigitalAddress> DigitalAddresses { get; set; }
+        public DbSet<DigitalAddressType> DigitalAddressTypes { get; set; }
+
+
+    }
+
+}
---
+++ DodoManufacturing/DodoBed.Manufacturing/DodoBed.Manufacturing.SqlPersistence/EntityConfigurations.cs
@@ -0,0 +1,54 @@
+﻿using DodoBed.Manufacturing.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodoBed.Manufacturing.SqlPersistence
+{
+    public class AddressTypeConfigurations : IEntityTypeConfiguration<DigitalAddressType>
+    {
+            /**
+     * This is where all the configurations for the entities are placed.
+     * These configurations are a way to step away from data annotations 
+     * being applied to the domain entities. Since, the core should remain as
+     * agnostic as possible.
+     * These configurations would not take effect if ApplyConfigurationsFromAssembly
+     * is not invoked from OnModelCreating method.
+     */
+        public void Configure(EntityTypeBuilder<DigitalAddressType> builder)
+        {
+            builder.Property(e => e.Name)
+                .HasConversion<int>();
+        }
+    }
+
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(e => e.CreatedDate)
+                 .HasDefaultValue(DateTime.Now);
+            builder.Property(e => e.LastModifiedDate)
+                  .HasDefaultValue(DateTime.Now);
+               
+        }
+    }
+
+    public class ToolConfiguration : IEntityTypeConfiguration<Tool>
+    {
+      
+        public void Configure(EntityTypeBuilder<Tool> builder)
+        {
+            builder.Property(e => e.CreatedDate)
+               .HasDefaultValue(DateTime.Now);
+            builder.Property(e => e.LastModifiedDate)
+                  .HasDefaultValue(DateTime.Now);
+        }
+    }
+
+
+}
