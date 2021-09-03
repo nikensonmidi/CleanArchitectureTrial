6e1ac2106949f2840525e16c59d8f138744fd6b0
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2021-09-03 15:44:49 -0400

    Infrastructure - persistence: added persistence registration

---
+++ DodoManufacturing/DodoBed.Manufacturing/DodoBed.Manufacturing.SqlPersistence/PersistenceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using DodoBed.Manufacturing.Application.Interfaces.Persistence;
+using DodoBed.Manufacturing.SqlPersistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class PersistenceRegistration
+    {
+
+        public static IServiceCollection AddSqlPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DodoBedManufacturingContext>(options => options.UseSqlServer(configuration["connectionStrings:dodobed_manufacturing"]));
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IProductRepository, ProductRepository>();
+            return services;
+        }
+    }
+}
