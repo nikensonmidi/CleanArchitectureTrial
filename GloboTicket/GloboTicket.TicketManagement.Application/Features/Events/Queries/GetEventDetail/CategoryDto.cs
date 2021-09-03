e134a7befec3e9f73624b365561ff9178bdc094d
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2021-09-03 15:47:06 -0400

    removed globotticket

--- GloboTicket/GloboTicket.TicketManagement.Application/Features/Events/Queries/GetEventDetail/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail
-{
-    using System;
-    public class CategoryDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-    }
-}
