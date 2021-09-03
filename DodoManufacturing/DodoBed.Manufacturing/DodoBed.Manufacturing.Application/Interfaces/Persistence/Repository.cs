d8b948437dd56433ec05e187be2e7e5c18bb1cdb
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2021-09-03 15:33:19 -0400

    Infrastrcuture - persistence: added repositories

---
+++ DodoManufacturing/DodoBed.Manufacturing/DodoBed.Manufacturing.Application/Interfaces/Persistence/Repository.cs
@@ -0,0 +1,29 @@
+﻿using DodoBed.Manufacturing.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodoBed.Manufacturing.Application.Interfaces.Persistence
+{
+    public interface IRepository<T> where T: class
+    {
+
+        IEnumerable<T>  GetAll();
+        T Add(T entity);
+        Task<T> AddAsync(T entity);
+        void Delete(T entity);
+        Task DeleteAsync(T entity);
+        T Update(T entity);
+        Task<T> UpdateAsync(T entity);
+
+    }
+
+    public interface IProductRepository:IRepository<Product>
+    {
+       
+    }
+
+
+}
---
+++ DodoManufacturing/DodoBed.Manufacturing/DodoBed.Manufacturing.SqlPersistence/Repositories.cs
@@ -0,0 +1,78 @@
+﻿using DodoBed.Manufacturing.Application.Interfaces.Persistence;
+using DodoBed.Manufacturing.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DodoBed.Manufacturing.SqlPersistence
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DodoBedManufacturingContext _dbContext;
+
+        public BaseRepository(DodoBedManufacturingContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public async  Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbContext.Set<T>().AsQueryable();
+        }
+
+        public T Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+             _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return entity;
+
+        }
+    }
+
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        private readonly DodoBedManufacturingContext _dbContext;
+        public ProductRepository(DodoBedManufacturingContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+       
+    }
+}
