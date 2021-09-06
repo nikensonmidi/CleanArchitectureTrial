d534a1127b82091aca551752e8f41d2b620acc39
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2021-09-06 13:50:17 -0400

    Manufactturing - Application: added custom execption

---
+++ DodoManufacturing/DodoBed.Manufacturing/DodoBed.Manufacturing.Application/Exceptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodoBed.Manufacturing.Application
+{
+    public class ValidationException:Exception
+    {
+        public ValidationException(string message):base(message)
+        {
+
+        }
+        public IEnumerable<string> ValidationErrors { get; set; }
+
+    }
+
+    public class NotFoundException:ApplicationException
+    {
+        public NotFoundException(string message):base(message)
+        {
+
+        }
+    }
+
+    public class BadRequestException:ApplicationException
+    {
+        public BadRequestException(string message):base(message)
+        {
+
+        }
+    }
+
+
+}
