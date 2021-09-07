1c1fc60ea92f7e6877e4f094edb4c22d84c8b5ab
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2021-09-07 11:02:18 -0400

    Test-benchamarks: added Product query benchmarks

---
+++ DodoManufacturing/DodoBed.Manufacturing/BenchMarks/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using DodoBed.Manufacturing.Application;
+using BenchMarks.Applicatrion;
+using BenchmarkDotNet.Running;
+using MediatR;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using DodoBed.Manufacturing.Application.Interfaces.Persistence;
+
+namespace BenchMarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            //var serviceProvider = new ServiceCollection()
+            //   .AddApplicationServices()
+            //   .AddSingleton<IProductRepository, ProductRepoMock>()
+            //   .AddMediatR(Assembly.GetExecutingAssembly())
+               
+            //   .BuildServiceProvider();
+
+          
+
+            var benchProduct = BenchmarkRunner.Run<ProductQuery>();
+
+        }
+    }
+}
